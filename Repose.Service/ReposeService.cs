54bc209be71b425e1694c57cbae87057c4adafc9
James Morgan <j.a.mrgn@gmail.com>
2014-01-16 14:52:23 -0500

    #1 - Added the Sln and a few references

---
+++ Repose.Service/ReposeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repose.Service
+{
+    public class ReposeService
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
